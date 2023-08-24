2c32d0f23f44df6a89159298ec99b0f11e55b22b
Cory Faber <cory.j.faber@gmail.com>
2023-08-24 09:34:34 -0500

    outline plan for the logic of the program

---
+++ javascript.js
@@ -0,0 +1,17 @@
+/* 
+Rock paper scissors game.
+
+
+Take input from user: choice of (rock, paper, scissors) and store the selection.
+turn player selection case insensitive
+If player selection is valid:
+    continue
+else:
+    display error and ask for a valid selection
+Generate a computer choice of rock, paper, scissors randomly and store the selection.
+
+Function to play one round and compare the player and computer selection, then return if the player won or lost the round.
+
+game() function that will use the previous one round function to play a 5 round game that keeps score and reports a winner or loser
+
+*/
